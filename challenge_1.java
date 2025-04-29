f0521fc51e2b7a56db91da2f3ff050f650599a74
JasonWachira <jason.kamwaro@strathmore.edu>
2025-04-29 13:53:21 +0300

    Commit

---
+++ challenge_1.java
@@ -0,0 +1,11 @@
+package Week_3_challenges;
+
+public class challenge_1 {
+    public static void greet(){
+        System.out.println("Hello there");
+
+    }
+    public static void main(String[] args){
+        challenge_1.greet();
+    }
+}
---
+++ challenge_6.java
@@ -0,0 +1,8 @@
+package Week_3_challenges;
+
+public class challenge_6 {
+
+    public void play(){
+
+    }
+}
