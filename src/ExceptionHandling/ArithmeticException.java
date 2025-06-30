671b33a7d5a5f7c1ab411f25eec3f7e580b034a4
JasonWachira <jason.kamwaro@strathmore.edu>
2025-06-30 11:45:12 +0300

    Exceptions

---
+++ src/ExceptionHandling/ArithmeticException.java
@@ -0,0 +1,20 @@
+package ExceptionHandling;
+import java.io.*;
+
+
+public class ArithmeticException {
+    public static void main(String[]args){
+        int n1 = 100;
+        int n2 = 0;
+        try {
+            System.out.println(n1 / n2);
+
+        }
+        catch(java.lang.ArithmeticException e){
+            System.out.println("Error "+e);
+        }
+        finally{
+            System.out.println("Program continues after the exception");
+        }
+    }
+}
